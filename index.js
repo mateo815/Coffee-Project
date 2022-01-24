c9148b09a5cf06fe2975f44f0571bad25728ba08
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-24 11:36:20 -0500

    header

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const baseURL = 'https://api.sampleapis.com/coffee/iced';
-fetch(baseURL)
+const coldURL = 'https://api.sampleapis.com/coffee/iced';
+const hotURL = 'https://api.sampleapis.com/coffee/hot';
+
+fetch(coldURL)
     .then(resp => resp.json())
     .then(data => console.log(data));
+
+
+fetch(hotURL)
+    .then(resp => resp.json())
+    .then(data => console.log(data));
+
